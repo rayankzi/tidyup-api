78cf93d66b7828ea026dd1609b8c98e79adc72be
Rayan Kazi <rayankazi7515@gmail.com>
2024-11-19 21:34:39 -0700

    added function and workflow

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,3 +1,7 @@
 export function sayHello(name: string | null = null): string {
   return `Hello, ${name || "World"}!`;
 }
+
+export function getAIRecommendations(treeRepresentation: string | null = null): string {
+  return `This is the tree representation ${treeRepresentation}`;
+}
